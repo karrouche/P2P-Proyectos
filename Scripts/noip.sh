237e79c4ceacd2b8b77cc0dedbe91ef9592ba013
Mohamed Mohand Karrouche <correo_moha@hotmail.com>
2015-05-21 19:38:21 +0200

    Update noip.sh
    Con esta actualización tendremos un menú de instalación de este servicio y podremos crear un script para que se inicie el servicio No-Ip al iniciar nuestro sistema.

--- Scripts/noip.sh
+++ Scripts/noip.sh
@@ -1,10 +1,56 @@
 #! /bin/bash
 #Mohamed Mohand Karrouche
-#Instalación de NO-IP
-wget http://www.no-ip.com/client/linux/noip-duc-linux.tar.gz
-tar -zxf noip-duc-linux.tar.gz
-cd noip-2.1.9-1/
-make
-sudo make install
-cd ..
-rm -r noip-duc-linux.tar.gz
+_MENU()
+{
+        echo "INDIQUE QUE OPCIÓN DESEE REALIZAR:"
+        echo
+    echo
+        echo "1) Instalar Servicio NO-IP."
+        echo "2) Crear el script para iniciar automaticamente."
+        echo "3) Salir."
+        echo -n "Indica una opcion: "
+}
+until [ "$opc" = "3" ];
+do
+        case $opc in
+                1)      wget http://www.no-ip.com/client/linux/noip-duc-linux.tar.gz
+                        tar -zxf noip-duc-linux.tar.gz
+                        cd noip-2.1.9-1/
+                        make
+                        sudo make install
+                        cd ..
+                        rm -r noip-duc-linux.tar.gz
+                                echo
+                echo
+                        _MENU
+                        ;;
+
+                2)      echo "#! /bin/bash
+### BEGIN INIT INFO
+# Provides: blabla
+# Required-Start: $syslog
+# Required-Stop: $syslog
+# Default-Start: 2 3 4 5
+# Default-Stop: 0 1 6
+# Short-Description: blabla
+# Description:
+#
+### END INIT INFO
+sudo /usr/local/bin/noip2" > /etc/init.d/noip2
+                        echo "Script creado correctamente"
+                        sleep 4
+            clear
+                        _MENU
+                        ;;
+
+                3) exit ;;
+
+                *)
+                clear
+                echo "Por favor indica una de las siguientes opciones"
+                _MENU
+                ;;
+
+        esac
+read opc
+done
